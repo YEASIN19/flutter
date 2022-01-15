5bf5048bf1321577408d905a9b92a0d14af75bc2
yeasin arafat <07yeasin1981@gmail.com>
2022-01-15 21:09:09 +0600

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(
+  ));
 }
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
       
     );
   }
